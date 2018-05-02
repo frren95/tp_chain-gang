a2c9dfdf8d98196223ceb6047df0947ea42326e8
Frédérique Renet <frederique.renet@free.fr>
2018-05-02 15:34:08 +0200

    Amélioration de la classe ParseCSV

--- public/bicycles.php
+++ public/bicycles.php
@@ -31,13 +31,10 @@
 $parser = new ParseCSV(PRIVATE_PATH . '/used_bicycles.csv');
 $bike_array = $parser->parse();
 
-print_r($bike_array);
-
-$args =['brand' => 'Trek', 'model' => 'Emonda', 'year' => 2017, 'gender' => 'unisex', 'color' => 'black', 'category' => 'Road', 'weight_kg' => 11.3, 'price' => 250.00];
-$bike = new Bicycle($args);
-
 ?>
-      <?php for($i=0; $i <=10; $i++){ ?>
+
+      <?php foreach($bike_array as $args) { ?>
+        <?php $bike = new Bicycle($args); ?>
       <tr>
         <td><?php echo h($bike->brand); ?></td>
         <td><?php echo h($bike->model); ?></td>
